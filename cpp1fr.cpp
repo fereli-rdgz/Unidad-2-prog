def3471b77056358294fecc127b19051a9b52f54
fereli-rdgz <ferelirdgz1826@gmail.com>
2024-10-11 09:21:43 -0500

    Add files via upload

---
+++ cpp1fr.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+cout <<"Hola este programa 1.6 escribe los datos en orden inverso" << "\n";
+int A,B,C,D;
+cout << "Porfavor ingrese el primer valor A:" << "\n";
+cin >> A;
+cout << "Porfavor ingrese el segundo valor B:" << "\n";
+cin >> B;
+cout << "Porfavor ingrese el tercer valor C:" << "\n";
+cin >> C;
+cout << "Porfavor ingrese el cuarto valor D:" << "\n";
+cin >> D;
+cout << D << "," << C << "," << B << "," << A;
+return 0;
+}
